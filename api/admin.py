8e4b654e320ab243f1ffdedfc43b395cff8ed41d
Alex Leiko <leiko.oleksandr@gmail.com>
2021-02-24 17:40:38 +0100

    intial models setting

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from api.models import Offer, Product
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'name', 'description')
+   
+admin.site.register(Product, ProductAdmin)
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('offer_id', 'price', 'items_in_stock')
+   
+admin.site.register(Offer, OfferAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    '''     
+    Products - Each product corresponds to a real world product you can buy 
+
+    id: The type is up to you :-) 
+    name : string 
+    description: string 
+    '''
+
+    product_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    class Meta:
+        verbose_name_plural = 'products'
+    
+    def __str__(self):
+        return self.name
+
+
+
+class Offer(models.Model):
+    '''     
+    Offers - Each offer represents a product offer being sold for some price somewhere 
+    
+    id: The type is up to you :-) 
+    price : integer 
+    items_in_stock: integer 
+    '''
+
+    offer_id = models.AutoField(primary_key=True)
+    price = models.PositiveIntegerField()
+    items_in_stock = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False)
+
+    class Meta:
+        verbose_name_plural = 'offers'
+    
+    def __str__(self):
+        return str(self.offer_id)
